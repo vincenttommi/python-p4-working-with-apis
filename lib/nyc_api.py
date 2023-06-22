98d44330d9e9cd8b1bc3c61b4818e0cfd4594f06
vincenttommi <vincenttommi@gmail.com>
2023-06-22 20:17:22 +0300

    working with apis

--- lib/nyc_api.py
+++ lib/nyc_api.py
@@ -1,15 +1,54 @@
-
 import requests
 import json
 
+
+# requests is a Python library
+# that allows your program 
+# or application to send HTTP requests.
+
+
 class GetPrograms:
+  
+  def get_programs():
+    # we use the JSON library to parse the API response into nicely formatted JSON
+     URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
+     
+     
+     response  = requests.get(URL)
+     return response.content
+   
+   
+   
+   
+  def  program_school(self):
+    
+    programs_list = []
+    # creating an empty list that will receive data
+    programs = json.loads(self.get_programs())
+    # declaring a avaribale  programs  that receives data when  json loads data from get_pograms function
+    for program in programs:
+      #introducing a loop that iterates over the variable programs and that  using append method to append it to program list
+      programs_list.append(program(["agency"]))
+      
+      return programs_list
+    #returning program_list to give out an output
+    
+    programs = GetPrograms()
+    programs_schools  = programs.program_school() 
+    
+    for school  in set(programs_schools):
+      print(school)
 
-  def get_programs(self):
-    URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-    response = requests.get(URL)
-    return response.content
+    
+   
+# Programs  = GetPrograms.get_programs()
+# # get_programs method that uses the
+# # requests library to send an HTTP request 
+# # from our program.
+# print(Programs)
 
+   
 
-programs = GetPrograms().get_programs()
-print(programs)
+     
+   
